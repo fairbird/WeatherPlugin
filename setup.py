24592d1bfe777e2b7428eef2a9673fadca7ea3f6
RAED <rrrr53@hotmail.com>
2023-09-05 23:33:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='enigma2-plugin-extensions-weatherplugin',
        version='2.1',
        description='Weather plugin',
        packages=[pkg],
-       package_dir={pkg: 'plugin'},
+       package_dir={pkg: 'usr'},
        package_data={pkg: ['weather.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass=setup_translate.cmdclass, # for translation
       )
